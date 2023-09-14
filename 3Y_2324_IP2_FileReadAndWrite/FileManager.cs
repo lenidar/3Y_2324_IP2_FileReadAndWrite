eb0e27b2b26380f17be41c4808b5985b93f424da
lenidar <55443074+lenidar@users.noreply.github.com>
2023-09-14 09:13:04 +0800

    Add project files.

---
+++ 3Y_2324_IP2_FileReadAndWrite/FileManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace _3Y_2324_IP2_FileReadAndWrite
+{
+    internal class FileManager
+    {
+        public bool fileCheck(string path)
+        {
+            if (File.Exists(path))
+                return true;
+            else
+                return false;
+        }
+
+        public List<string> readFile(string path)
+        {
+            List<string> lines = new List<string>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line = string.Empty;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            return lines;
+        }
+
+        public void writeFile(string path, string message) 
+        {
+            string[] lines = message.Split('\n');   
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (string line in lines) 
+                {
+                    sw.WriteLine(line);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ 3Y_2324_IP2_FileReadAndWrite/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3Y_2324_IP2_FileReadAndWrite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileManager fm = new FileManager();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbFilePath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(tbFilePath.Text.Length > 0) 
+            {
+                btnIOManage.IsEnabled = true;
+                if(fm.fileCheck(tbFilePath.Text)) 
+                {
+                    btnIOManage.Content = "Open File";
+                }
+                else
+                {
+                    btnIOManage.Content = "Create File";
+                }
+            }
+            else
+            {
+                btnIOManage.IsEnabled = false;
+            }
+        }
+
+        private void btnIOManage_Click(object sender, RoutedEventArgs e)
+        {
+            if(btnIOManage.Content.ToString() == "Open File")
+            {
+                List<string> things = new List<string>(); 
+
+                things = fm.readFile(tbFilePath.Text);
+
+                foreach(string thing in things) 
+                {
+                    tbPlaceToReadWrite.Text += thing + "\n";
+                }
+            }
+            else
+            {
+                //MessageBox.Show(tbPlaceToReadWrite.Text);
+                fm.writeFile(tbFilePath.Text, tbPlaceToReadWrite.Text);
+            }
+        }
+    }
+}
